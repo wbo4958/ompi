2b388661bb5a0d30330d7bd0344525215a515258
Bobby Wang <wbo4958@gmail.com>
2020-09-26 20:33:10 +0800

    update

--- compile.sh
+++ compile.sh
@@ -3,6 +3,7 @@
 
 # you may need to install flex by sudo apt install flex
 ./autogen.pl
+mkdir -p /home/bobwang/tools/library/ompi
 ./configure --prefix=/home/bobwang/tools/library/ompi
 make all
 make install
