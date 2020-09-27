ff1880b0a6743e9449154f89591f328dfeca575b
Bobby Wang <wbo4958@gmail.com>
2020-09-27 08:13:57 +0800

    update

--- compile.sh
+++ compile.sh
@@ -3,6 +3,6 @@
 
 # you may need to install flex by sudo apt install flex
 ./autogen.pl
-./configure --prefix=/home/bobwang/tools/library/ompi
+./configure --prefix=/home/bobwang/work.d/nvspark/library/ompi
 make all
 make install
